b8829c1ae153c798f5f6eeeaee5dfba8fc39741f
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2022-10-11 09:28:38 -0500

    init

---
+++ src/labs/Lab02.java
@@ -0,0 +1,38 @@
+package labs;
+
+import java.util.Scanner;
+
+public class Lab02 {
+
+	public static void main(String[] args) {
+
+		problem01();
+		problem02();
+		problem03();
+
+	}
+
+
+	public static void problem01() {
+		
+		
+		
+	}
+
+
+	public static void problem02() {
+		
+		
+		
+		
+	}
+
+
+	public static void problem03() {
+
+		
+		
+	}
+
+
+}
